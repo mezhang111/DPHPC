0d6d8611d1093b760bc99fba6d8804bebf811ef1
mezhang111 <91820741+mezhang111@users.noreply.github.com>
2021-11-08 19:14:27 +0100

    Add files via upload
    naive

--- xilinx/Example1.cpp
+++ xilinx/Example1.cpp
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <math.h> 
-constexpr int N = 1024;
+constexpr int N = 64;
 
 void gramschmidt_naive(float *A, float *Q, float *R) {
+
   for(int k=0; k<N; k++){
     float dot_product = 0.0;
     for(int j=0; j<N; j++){
-      #pragma HLS PIPELINE II=1
       float val = A[j*N+k];
       dot_product += val*val;   //calculate dot product of the i-th column. Can be optimized. Data Dependencies + column-major accesses
+      
     }
     
     R[k*N+k] = sqrt(dot_product);
     
     for(int j=0; j<N; j++){
-      #pragma HLS PIPELINE II=1
       Q[j*N+k] = A[j*N+k]/R[k*N+k];
     }
     for(int j=k;j<N; j++){ //Note that the report is not able to caputure the value of k at compile time. Therefore there will be min and max.
       dot_product = 0.0   //BUT WE CAN STILL CALCULATE THE CORRECT LOOP LETENCY BY LINEAR INTERPOLATING.
       for(int i=0; i<N; i++){
-        #pragma HLS PIPELINE II=2
         float val = Q[i*N+k];
         float val2 = A[i*N+j];
         dot_product += val*val2;
